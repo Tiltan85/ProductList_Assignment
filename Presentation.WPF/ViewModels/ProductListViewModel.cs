f03455924e4cbf56be28be61315cc5e53bb01d59
Tiltan85 <tiltan85@gmail.com>
2025-10-08 20:31:16 +0200

    Layout and navigate to ProductAddView added.

--- Presentation.WPF/ViewModels/ProductListViewModel.cs
+++ Presentation.WPF/ViewModels/ProductListViewModel.cs
@@ -1,12 +1,32 @@
 ﻿
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.ObjectModel;
 
 namespace Presentation.WPF.ViewModels;
 
 public partial class ProductListViewModel : ObservableObject
 {
 
+    private readonly IServiceProvider _serviceProvider;
+
+    public ProductListViewModel(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
     [ObservableProperty]
     private string _title = "Product List";
 
+    [ObservableProperty]
+    private ObservableCollection<Product> _productList = [];
+
+    [RelayCommand]
+    private void NavigateToAddView()
+    {
+        var mainViewModel = _serviceProvider.GetRequiredService<MainViewModel>();
+        mainViewModel.CurrentViewModel = _serviceProvider.GetRequiredService<ProductAddViewModel>();
+    }
 }
