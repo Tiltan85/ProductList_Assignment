f8a2bea3c6342d0867e5cd83ceffcf9f01bf2ebb
Tiltan85 <tiltan85@gmail.com>
2025-10-08 13:14:32 +0200

    Done

--- Presentation.Console/Program.cs
+++ Presentation.Console/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Infrastructure.Repositories;
+using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+IHost host = Host.CreateDefaultBuilder()
+    .ConfigureServices(services => 
+    {
+        services.AddSingleton<IJsonFileRepository>(new JsonFileRepository("products.json"));
+        services.AddSingleton<IProductService, ProductService>();
+    })
+    .Build();
+
