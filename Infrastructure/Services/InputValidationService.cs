b095836835eeadfdada036a8306f7b0ea9b48b17
Tiltan85 <tiltan85@gmail.com>
2025-10-12 00:05:39 +0200

    Input validation for add product is now working

--- Infrastructure/Services/InputValidationService.cs
+++ Infrastructure/Services/InputValidationService.cs
@@ -49,4 +49,45 @@ public class InputValidationService : IInputValidationService
 
         return result;
     }
+
+    public InputResult VerifyProductForm(ProductRequest productRequest)
+    {
+        var result = new InputResult();
+
+        if (productRequest == null)
+        {
+            result.Success = false;
+            result.StatusCode = 400; // 400 Bad Request, Fel input från användaren
+            result.Error = "Product edit form is empty";
+            return result;
+        }
+        if (string.IsNullOrWhiteSpace(productRequest.ProductName))
+            result.FieldErrors.Add(new InputError { Field = "Name", Message = "Product name can't be empty." });
+
+        if (string.IsNullOrWhiteSpace(productRequest.ProductDescription))
+            result.FieldErrors.Add(new InputError { Field = "Description", Message = "Product description can't be empty." });
+
+        if (string.IsNullOrWhiteSpace(productRequest.Category))
+            result.FieldErrors.Add(new InputError { Field = "Category", Message = "Product category can't be empty." });
+
+        if (string.IsNullOrWhiteSpace(productRequest.Manufacturer))
+            result.FieldErrors.Add(new InputError { Field = "Manufacturer", Message = "Product manufacturer can't be empty." });
+
+        if (productRequest.ProductPrice < 0)
+            result.FieldErrors.Add(new InputError { Field = "Price", Message = "Product pricec can't be negative value." });
+
+        if (result.FieldErrors.Count > 0)
+        {
+            result.Success = false;
+            result.StatusCode = 400;
+            result.Error = "Fields have errors.";
+        }
+        else
+        {
+            result.Success = true;
+            result.StatusCode = 204;
+        }
+
+        return result;
+    }
 }
