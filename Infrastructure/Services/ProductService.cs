b1984a943cb37a4bb588330fd29fc0459ea10d63
Tiltan85 <tiltan85@gmail.com>
2025-10-11 23:39:12 +0200

    Validation only works for Edit.

--- Infrastructure/Services/ProductService.cs
+++ Infrastructure/Services/ProductService.cs
@@ -13,7 +13,6 @@ public interface IProductService
     Task<ProductResult> SaveProductAsync(ProductRequest productRequest, CancellationToken cancellationToken = default);
     Task<ProductResult> EditProductAsync(Product product, CancellationToken cancellationToken = default);
     Task<ProductResult> DeleteProductAsync(Product product, CancellationToken cancellationToken = default);
-    Task<ProductResult> VerifyProductFormInput(ProductRequest productRequest);
 }
 
 public class ProductService(IJsonFileRepository jsonFileRepository, IInputValidationService inputValidationService) : IProductService
@@ -105,6 +104,8 @@ public class ProductService(IJsonFileRepository jsonFileRepository, IInputValida
                 Manufacturer = new Manufacturer { ManufacturerName = productRequest.Manufacturer },
             };
 
+            // TODO ProductDuplicationCheck (kolla så där inte redan finns en product med samma Id, och namn)
+
             _products.Add(product);
             await _jsonFileRepository.WriteAsync(_products, cancellationToken);
 
@@ -118,33 +119,41 @@ public class ProductService(IJsonFileRepository jsonFileRepository, IInputValida
 
     public async Task<ProductResult> EditProductAsync(Product product, CancellationToken cancellationToken = default)
     {
-        if (product == null)
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Invalid Input" }; // 400 Bad Request, Fel input från användaren
 
-        try
+        var result = _inputValidationService.VerifyProductForm(product);
+
+        if (result.Success)
         {
-            var existing_product = await GetProductByIdAsync(product.Id, cancellationToken);
-
-            if (existing_product.Content is not null)
+            try
             {
-                existing_product.Content.ProductName = product.ProductName;
-                existing_product.Content.ProductDescription = product.ProductDescription;
-                existing_product.Content.Category = product.Category;
-                existing_product.Content.Manufacturer = product.Manufacturer;
-                existing_product.Content.ProductPrice = product.ProductPrice;
+                var existing_product = await GetProductByIdAsync(product.Id, cancellationToken);
 
-                await _jsonFileRepository.WriteAsync(_products, cancellationToken);
+                if (existing_product.Content is not null)
+                {
+                    existing_product.Content.ProductName = product.ProductName;
+                    existing_product.Content.ProductDescription = product.ProductDescription;
+                    existing_product.Content.Category = product.Category;
+                    existing_product.Content.Manufacturer = product.Manufacturer;
+                    existing_product.Content.ProductPrice = product.ProductPrice;
 
-                return new ProductResult { Success = true, StatusCode = 204 }; // 204 Allt gick som det ska, skickar inte tillbaka något
+                    // TODO ProductDuplicationCheck (kolla så där inte redan finns en product med samma Id, och namn)
+
+                    await EnsureLoadedAsync(cancellationToken);
+                    await _jsonFileRepository.WriteAsync(_products, cancellationToken);
+
+                    return new ProductResult { Success = true, StatusCode = 204 }; // 204 Allt gick som det ska, skickar inte tillbaka något
+                }
+
+                return new ProductResult { Success = false, StatusCode = 404, Error = "Product not found" };
+
             }
-
-            return new ProductResult { Success = false, StatusCode = 404, Error = "Product not found" };
-
+            catch (Exception ex)
+            {
+                return new ProductResult { Success = false, StatusCode = 500, Error = ex.Message }; // 500 generellt felmeddelande.
+            }
         }
-        catch (Exception ex)
-        {
-            return new ProductResult { Success = false, StatusCode = 500, Error = ex.Message }; // 500 generellt felmeddelande.
-        }
+
+        return new ProductResult { Success = false, StatusCode = result.StatusCode, Error = result.Error, FieldErrors = result.FieldErrors};
     }
 
     public async Task<ProductResult> DeleteProductAsync(Product product, CancellationToken cancellationToken = default)
@@ -166,28 +175,5 @@ public class ProductService(IJsonFileRepository jsonFileRepository, IInputValida
         {
             return new ProductResult { Success = false, StatusCode=500, Error = ex.Message };
         }
-    }
-
-    public async Task<ProductResult> VerifyProductFormInput(ProductRequest productRequest)
-    {
-        if (productRequest == null)
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Invalid Input" }; // 400 Bad Request, Fel input från användaren
-
-        if (string.IsNullOrWhiteSpace(productRequest.ProductName))
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Product name can't be empty" };
-
-        if (string.IsNullOrWhiteSpace(productRequest.ProductDescription))
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Product description can't be empty" };
-
-        if (string.IsNullOrWhiteSpace(productRequest.Category))
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Product category can't be empty" };
-
-        if (string.IsNullOrWhiteSpace(productRequest.Manufacturer))
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Product manufacturer can't be empty" };
-
-        if (productRequest.ProductPrice < 0)
-            return new ProductResult { Success = false, StatusCode = 400, Error = "Product price can't be negative" };
-
-        return new ProductResult { Success = true, StatusCode = 204 };
     }
 }
