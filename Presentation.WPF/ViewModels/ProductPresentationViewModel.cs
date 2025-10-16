cd1948420110df9f0b00610a3162b30300158805
Tiltan85 <tiltan85@gmail.com>
2025-10-16 14:26:14 +0200

    Product Presentation View and a basic layout created. ProductPresentationViewModel create.

---
+++ Presentation.WPF/ViewModels/ProductPresentationViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Infrastructure.Models;
+using System.Collections.ObjectModel;
+
+namespace Presentation.WPF.ViewModels;
+
+public class ProductPresentationViewModel : ObservableObject
+{
+    public ObservableCollection<Product> Products { get; set; } = [];
+    public Product? SelectedProduct { get; set; }
+}
