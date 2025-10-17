67c1633f02eb440d2d8448882450f6663de7619e
Tiltan85 <tiltan85@gmail.com>
2025-10-17 17:03:55 +0200

    Removed Guid, added new() to Category and Manufacturer

--- Infrastructure/Models/Product.cs
+++ Infrastructure/Models/Product.cs
@@ -3,10 +3,10 @@ namespace Infrastructure.Models;
 
 public class Product
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; } = null!;
     public string ProductName { get; set; } = null!;
     public string ProductDescription { get; set; } = null!;
-    public Category Category { get; set; } = null!;
-    public Manufacturer Manufacturer { get; set; } = null!;
+    public Category Category { get; set; } = new();
+    public Manufacturer Manufacturer { get; set; } = new();
     public decimal ProductPrice { get; set; }
 }
